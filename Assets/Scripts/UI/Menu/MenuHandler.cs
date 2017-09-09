f04da79af7935702ee67a4e9c62e6a48bdffe033
Julian Spittel <julispittel@googlemail.com>
2017-09-09 13:27:41 +0200

    Basic menuing

---
+++ Assets/Scripts/UI/Menu/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+    public void OnQuit()
+    {
+#if DEBUG
+        Debug.Break();
+#else
+        Application.Stop();
+#endif
+    }
+}
