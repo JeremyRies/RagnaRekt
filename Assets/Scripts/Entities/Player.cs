3aa65d40eb808884e8400874de6759c58e79653d
Philipp Rabe <philipp.rabe@fluffyfairygames.com>
2017-09-09 16:38:50 +0200

    Added Spawning GameLogic.

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿using Control;
+using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private PlayerControllerBase _controller;
+        [SerializeField] private SpriteRenderer _sprite;
+
+        public int PlayerId { set { _controller.PlayerId = value; } }
+        public void LookLeft() { _controller.UpdateViewDirection(new Vector2(-1,0)); }
+        public Color Color { set { _sprite.color = value; } }
+    }
+}
---
+++ Assets/Scripts/GameLogic/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts.Entities;
+using Control;
+using UnityEngine;
+
+namespace LifeSystem
+{
+    public class GameConfig : ScriptableObject
+    {
+        public int AmountOfPlayers = 2;
+        public Player PlayerPrefab;
+    }
+}
