78ee802128446c73891b16e800f6132499b2584f
Philipp Rabe <philipp.rabe@fluffyfairygames.com>
2017-09-09 13:24:40 +0200

    First attempt for a melee attack. SetVisible does not work

---
+++ Assets/Scripts/Control/Actions/MeleeAttack.cs
@@ -0,0 +1,38 @@
+﻿using System.Timers;
+using Assets.Scripts.Entities;
+using Assets.Scripts.Util;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+
+namespace Control.Actions
+{
+    public class MeleeAttack : Action
+    {
+        [SerializeField] private float _cooldownTimeInSeconds = 2;
+        [SerializeField] private float _attackTimeInSeconds = 1;
+        [SerializeField] private Sword _sword;
+
+        private Cooldown _cooldown;
+
+        private void Start()
+        {
+            _cooldown = new Cooldown(_cooldownTimeInSeconds);
+        }
+
+        public override void TryToActivate(Direction direction)
+        {
+            if (_cooldown.IsOnCoolDown) return;
+
+            Attack();
+            _cooldown.Start();
+        }
+
+        private void Attack()
+        {
+            _sword.Visible = true;
+            var timer = new Timer(_attackTimeInSeconds);
+            timer.Elapsed += (nil, args) => _sword.Visible = false;
+            timer.Start();
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Sword.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Entities
+{
+    public class Sword : MonoBehaviour
+    {
+        private GameObject _gameObject;
+
+        private void Start()
+        {
+            _gameObject = gameObject;
+        }
+
+        public bool Visible { set { _gameObject.SetActive(value); } } 
+
+
+    }
+}
