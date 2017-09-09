6c2231e6cd979529a073d8874a94731006c6474e
Philipp Rabe <philipp.rabe@fluffyfairygames.com>
2017-09-09 12:57:29 +0200

    Added Cooldown Timer

---
+++ Assets/Scripts/Util/Cooldown.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Timers;
+using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    public class Cooldown
+    {
+        private readonly float _cooldownTime;
+
+        private Timer _timer;
+        private bool _onCooldown;
+
+        public bool IsOnCoolDown { get { return _onCooldown; } }
+
+        public event Action IsOffCooldown;
+
+        public Cooldown(float cooldownTimeInSeconds)
+        {
+            _cooldownTime = cooldownTimeInSeconds;
+            ResetTimer();
+        }
+
+        private void ResetTimer()
+        {
+            _timer = new Timer(_cooldownTime * 1000);
+            _timer.Elapsed += WhenTimerElapsed;
+            _timer.BeginInit();
+        }
+
+        public void Start()
+        {
+            _onCooldown = true;
+            _timer.Start();
+        }
+
+        private void WhenTimerElapsed(object nil, ElapsedEventArgs args)
+        {
+            _onCooldown = false;
+            if (IsOffCooldown != null) IsOffCooldown();
+            ResetTimer();
+        }
+    }
+}
