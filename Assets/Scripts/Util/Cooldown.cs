718910d8a12fbb902c52ee02c3a8127b85697cdc
Philipp Rabe <philipp.rabe@fluffyfairygames.com>
2017-09-09 15:08:17 +0200

    Rectified the Cooldown Timer

--- Assets/Scripts/Util/Cooldown.cs
+++ Assets/Scripts/Util/Cooldown.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Timers;
+using UniRx;
 using UnityEngine;
 
 namespace Assets.Scripts.Util
@@ -8,37 +9,21 @@ namespace Assets.Scripts.Util
     {
         private readonly float _cooldownTime;
 
-        private Timer _timer;
-        private bool _onCooldown;
-
-        public bool IsOnCoolDown { get { return _onCooldown; } }
-
-        public event Action IsOffCooldown;
+        private readonly ReactiveProperty<bool> _onCooldown = new ReactiveProperty<bool>(false);
+        public ReactiveProperty<bool> IsOnCoolDown { get { return _onCooldown.ToReactiveProperty(); } }
 
         public Cooldown(float cooldownTimeInSeconds)
         {
             _cooldownTime = cooldownTimeInSeconds;
-            ResetTimer();
-        }
-
-        private void ResetTimer()
-        {
-            _timer = new Timer(_cooldownTime * 1000);
-            _timer.Elapsed += WhenTimerElapsed;
-            _timer.BeginInit();
         }
 
         public void Start()
         {
-            _onCooldown = true;
-            _timer.Start();
-        }
+            if (_onCooldown.Value) return;
 
-        private void WhenTimerElapsed(object nil, ElapsedEventArgs args)
-        {
-            _onCooldown = false;
-            if (IsOffCooldown != null) IsOffCooldown();
-            ResetTimer();
+            _onCooldown.Value = true;
+            Observable.Timer(TimeSpan.FromSeconds(_cooldownTime))
+                .Subscribe(_ => _onCooldown.Value = false);
         }
     }
 }
