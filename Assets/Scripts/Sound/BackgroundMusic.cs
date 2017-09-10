f50885196fcb91691d43467275eabc0832bddeec
Julian Spittel <julispittel@googlemail.com>
2017-09-10 14:18:10 +0200

    Added BackgroundMusic Player

--- Assets/Scripts/Sound/BackgroundMusic.cs
+++ Assets/Scripts/Sound/BackgroundMusic.cs
@@ -1,19 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace Sound
 {
     public class BackgroundMusic : MonoBehaviour
     {
         [SerializeField]
-        private AudioClip _clip;
+        private SfxClip[] _clips;
         [SerializeField]
         private AudioSource _audioSource;
+        
+        private static BackgroundMusic _backgroundMusicInstance;
 
+        public static BackgroundMusic BackgroundMusicInstance
+        {
+            get { return _backgroundMusicInstance; }
+        }
         private void Start()
         {
+            _backgroundMusicInstance = this;
             _audioSource.loop = true;
-            _audioSource.clip = _clip;
+        }
+
+        public void SetClip(ClipIdentifier clipIdentifier)
+        {
+            var clips = _clips.Where(clip => clip.Clip == clipIdentifier).ToArray();
+            var rand = Random.Range(0, clips.Length);
+            var sfxClip = clips[rand];
+            _audioSource.clip = sfxClip.AudioClip;
+        }
+
+        public void StartPlay()
+        {
             _audioSource.Play();
+        }
+
+        public void StopPlay()
+        {
+            _audioSource.Stop();
         }
     }
 }
